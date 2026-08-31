900250a36d1f30d35ccdc96845711dbb7d3a0012
Chen Chen <0109chenchen@gmail.com>
2026-08-31 14:03:43 +0800

    Solve pytorch-activation-function-from-scratch

--- study-plans/pytorch-basics/pytorch-activation-function-from-scratch/pytorch-activation-function-from-scratch.py
+++ study-plans/pytorch-basics/pytorch-activation-function-from-scratch/pytorch-activation-function-from-scratch.py
@@ -6,18 +6,18 @@ def activate(x, method="relu"):
     """
     x = torch.tensor(x, dtype=torch.float32)
     if method == "relu":
-        # res = torch.clamp(x, min=0)
-        res = torch.nn.functional.relu(x)
+        res = torch.clamp(x, min=0)
+        # res = torch.nn.functional.relu(x)
     elif method == "sigmoid":
-        # res = 1.0 / (1.0 + torch.exp(-x))
-        res = torch.nn.functional.sigmoid(x)
+        res = 1.0 / (1.0 + torch.exp(-x))
+        # res = torch.nn.functional.sigmoid(x)
     elif method == "tanh":
-        # e1 = torch.exp(x)
-        # e_ = torch.exp(-x)
-        # res = (e1 - e_) / (e1 + e_)
-        res = torch.nn.functional.tanh(x)
+        e1 = torch.exp(x)
+        e_ = torch.exp(-x)
+        res = (e1 - e_) / (e1 + e_)
+        # res = torch.nn.functional.tanh(x)
     elif method == "leaky_relu":
-        # res = torch.where(x>0, x, 0.01*x)
-        res = torch.nn.functional.leaky_relu(x)
+        res = torch.where(x>0, x, 0.01*x)
+        # res = torch.nn.functional.leaky_relu(x)
 
     return res.tolist()
