5934bc3cea60e5602b8e14403b7aa4372cbdd261
Chen Chen <0109chenchen@gmail.com>
2026-08-31 11:55:29 +0800

    Solve pytorch-activation-function-from-scratch

---
+++ study-plans/pytorch-basics/pytorch-activation-function-from-scratch/pytorch-activation-function-from-scratch.py
@@ -0,0 +1,20 @@
+import torch
+
+def activate(x, method="relu"):
+    """
+    Returns: list (activated tensor converted via .tolist())
+    """
+    x = torch.tensor(x, dtype=torch.float32)
+    if method == "relu":
+        res = torch.clamp(x, min=0)
+    elif method == "sigmoid":
+        res = 1.0 / (1.0 + torch.exp(-x))
+    elif method == "tanh":
+        e1 = torch.exp(x)
+        e_ = torch.exp(-x)
+        res = (e1 - e_) / (e1 + e_)
+    elif method == "leaky_relu":
+        res = torch.where(x>0, x, 0.01*x)
+        # res = (x > 0) * x + (x <= 0) * 0.001 * x
+
+    return res.tolist()
